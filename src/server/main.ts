fbcb2c9e6860c6fd90899d2bdf181061a8742400
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2022-11-11 19:48:55 +0700

    Change to Fastify

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { ServerModule } from 'src/server/server.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ServerModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ServerModule,
+    new FastifyAdapter()
+  );
   await app.listen(3000);
 }
 bootstrap();
